91b44cb96f4191613315cb285d958b988b7dc9ee
Blasterphantom <blasterneko@gmail.com>
2023-06-23 14:29:14 -0700

    routing started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomecompComponent } from './homecomp/homecomp.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomecompComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
